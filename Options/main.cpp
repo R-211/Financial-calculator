594370b42cd2bfe22ae5a10b45e9990de0fae6ed
R <175558419+R-211@users.noreply.github.com>
2024-08-30 18:37:36 +0200

    Update main.cpp

--- Options/main.cpp
+++ Options/main.cpp
@@ -2,31 +2,31 @@
 
 #include "options.h"
 
+BlackScholesParams bs_params
+{
+	0.0,
+	0.0,
+	0.0,
+	0.0,
+	0.0,
+	OptionType::Call,
+	0.0
+};
+
+MonteCarloParams mtc_params
+{
+	1'000'000,
+	0.0,
+	0.0,
+	0.0,
+	0.0,
+	0.0,
+	OptionType::Call,
+	0.0
+};
+
 int main(int agrc, char** argv)
 {
-	BlackScholesParams bs_params
-	{
-		0.2,
-		100.0,
-		105.0,
-		0.5,
-		0.3,
-		OptionType::Call,
-		5.0
-	};
-
-	MonteCarloParams mtc_params
-	{
-		1'000'000,
-		0.2,
-		100.0,
-		105.0,
-		0.5,
-		0.3,
-		OptionType::Call,
-		5.0
-	};
-
 	FinancialCalculator financial_calculator;
 	std::cout << financial_calculator.calculateBlackScholes(bs_params) << std::endl;
 	std::cout << financial_calculator.calculateMonteCarlo(mtc_params) << std::endl;
